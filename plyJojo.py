b418c9cb05d9cba9609070d3b5dd73738fad1d3c
cesardelabarreda <cesar04_95@hotmail.com>
2017-02-27 01:29:28 -0600

    Se agregaron tokens
    palabras reservadas, tokens y reglas

---
+++ plyJojo.py
@@ -0,0 +1,117 @@
+import ply.lex as lex
+
+reserved = {
+   'if' : 'IF',
+   'else' : 'ELSE',
+   'while' : 'WHILE',
+   'true' : 'TRUE',
+   'false' : 'FALSE',
+   'neutral' : 'NEUTRAL',
+   'int' : 'INT',
+   'real' : 'REAL',
+   'string' : 'STRING',
+   'bool' : 'BOOL',
+   'tril' : 'TRIL',
+   'stand' : 'STAND',
+   'public' : 'PUBLIC',
+   'private' : 'PRIVATE',
+   'func' : 'FUNC',
+   'class' : 'CLASS',
+   'jojo' : 'JOJO',
+   'size' : 'SIZE',
+   'gets_int' : 'GETS_INT',
+   'gets_real' : 'GETS_REAL',
+   'gets_string' : 'GETS_STRING',
+   'gets_bool' : 'GETS_BOOL',
+   'prints_int' : 'PRINTS_INT',
+   'prints_real' : 'PRINTS_REAL',
+   'prints_string' : 'PRINTS_STRING',
+   'prints_bool' : 'PRINTS_BOOL',
+   'new' : 'NEW', 
+   'zadust' : 'ZADUST',
+   'ref' : 'REF',
+   'hamon' : 'HAMON',
+   'zawarudo' : 'ZAWARUDO',  
+}
+
+
+tokens = [
+   'PARI',
+   'PARD',
+   'LLAVEI',
+   'LLAVED',
+   'CORCHI',
+   'CORCHD',
+   'COMA',
+   'PUNTOYCOMA',
+   'PUNTO',
+   'COMP',
+   'AND',
+   'SUMRES',
+   'MULDIV',
+   'NOT',
+   'OR',
+   'IGUAL',   
+   'CTEI',
+   'CTER',
+   'CTES',
+   'CTEB',
+   'ID',
+] + list(reserved.values())
+
+
+t_PARI  	  	= r'\('
+t_PARD  	  	= r'\)'
+t_LLAVEI	 	= r'{'
+t_LLAVED	 	= r'}'
+t_CORCHI 		= r'\['
+t_CORCHD 		= r'\]'
+t_COMA 			= r','
+t_PUNTOYCOMA 	= r';'
+t_PUNTO 		= r'.'
+t_COMP 			= r'[\<|\>|==|\<=|\>=|\<=|!=]'
+t_AND 			= r'&&'
+t_SUMRES 		= r'[\+|-]'
+t_MULDIV 		= r'[\*|\/|%]'
+t_NOT 			= r'!'
+t_OR 			= r'\|\|'
+t_IGUAL 		= r'='
+t_CTES 			= r'\".*\"'
+
+
+def t_CTER(t):
+    r'([0-9]+[.])[0-9]+'
+    t.value = float(t.value)
+    return t
+
+def t_CTEI(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+def t_CTEB(t):
+    r'[true | false]'
+    t.value = bool(t.value)
+    return t
+
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value,'ID')
+    return t
+
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+
+t_ignore  = ' \t'
+
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+
+lexer = lex.lex()
+
