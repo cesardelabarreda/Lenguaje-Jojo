5096ab3b38e0b3edad145542713175fbda4e10e5
Carlos Googles <carlitox_garza@hotmail.com>
2017-04-28 16:33:57 -0500

    Errores y funcionalidad
    Se corrigio la funcion setMemVar para que se asigne correctamente a los
    ID de las variables globales. Tambien se definio un metodo isLocal, para
    saber si una variable es local o no.

--- Classes/DicFunc.py
+++ Classes/DicFunc.py
@@ -105,7 +105,7 @@ class DicFunction:
 
   def setMemVar(self, functionId, varId, mem):
     if self.existsFunction(functionId) == 0:
-      return 0
+      return self.functions["_Global"].setMemVar(varId, mem)
     return self.functions[functionId].setMemVar(varId, mem)
 
   def setMemFunc(self, functionId, mem):
@@ -132,6 +132,11 @@ class DicFunction:
     if self.existsFunction(functionId) == 0:
       return -1
     return self.functions[functionId].getQuadInicial()
+
+  def isLocal(self, functionId, varId):
+    if self.existsFunction(functionId) == 0:
+      return 0
+    return self.functions[functionId].existsVar(varId)
 
   def pprint(self):
     iTam = self.size()
