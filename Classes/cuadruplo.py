c5c8cdf1c9609b9182255e40af003a4a0fca93cb
Carlos Googles <carlitox_garza@hotmail.com>
2017-05-02 10:36:48 -0500

    Print2 para mostrar los quads

--- Classes/cuadruplo.py
+++ Classes/cuadruplo.py
@@ -60,3 +60,34 @@ class Quadruple:
 						print(" "),
 			print("]")
 			iNum = iNum + 1
+
+	def pprint2(self):
+		typeConv = TypeConvertion()
+		iNum = 0
+		for quad in self.quads:
+			print(str(iNum) + "\t["),
+			
+			print(str(typeConv.convertOp(quad[0]))),
+			iTam = len(str(typeConv.convertOp(quad[0])))
+			for j in range(0, 3):
+				print(" "),
+
+			for i in range(1, 4):
+				if quad[i] == None:
+					print(str(quad[i])),
+					iTam = 14 - len(str(quad[i]))
+					if (iTam % 2 == 1):
+						iTam = iTam - 3
+						print("  "),
+					for j in range(0, (iTam / 2)):
+						print(" "),
+				else:
+					print(str(quad[i])),
+					iTam = 14 - len(str(sVal))
+					if (iTam % 2 == 1):
+						iTam = iTam - 3
+						print("  "),
+					for j in range(0, (iTam / 2)):
+						print(" "),
+			print("]")
+			iNum = iNum + 1
